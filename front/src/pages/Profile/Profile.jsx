7bb8acf529139fb01c668b45aa10e23a41b30926
engywahab <132269909+GigiMW@users.noreply.github.com>
2025-03-04 22:18:01 +0200

    update profile a sign out button

--- front/src/pages/Profile/Profile.jsx
+++ front/src/pages/Profile/Profile.jsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom"
 
 
 const Profile = () => {
   return (
+    <>
     <div>Profile</div>
+    <Link to="/" className="mt-2 inline-block bg-[#1034d7] text-white px-10 py-7 rounded-lg font-semibold text-xl hover:bg-blue-700">
+        Sign Out
+      </Link>
+      
+    </>
+    
   )
 }
 
