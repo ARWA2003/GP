94e4878ee6c731b6aa5de15323c68ce5bca1fe38
engywahab <132269909+GigiMW@users.noreply.github.com>
2025-03-10 13:24:02 +0200

    remove career tips

--- front/src/pages/Education/Education.jsx
+++ front/src/pages/Education/Education.jsx
@@ -5,7 +5,7 @@ import Foooter from "../footer/footer";
 
 const services = [
     { title: "Online courses", image: "/assets/edu2.png", link: "/Courses", bg: "bg-blue-100" , description: "Links to free courses in various fields with subtitels or with  sign language explanations.", imgSize: " w-48 h-48"},
-    { title: "Career Preparation", image: "/assets/edu5.png", link: "/Careerhome", bg: "bg-blue-100" , description: "Interview Preparation,Resume templete Writing ,Soft Skills tips.", imgSize: "w-48 h-48"},
+    // { title: "Career Preparation", image: "/assets/edu5.png", link: "/Careerhome", bg: "bg-blue-100" , description: "Interview Preparation,Resume templete Writing ,Soft Skills tips.", imgSize: "w-48 h-48"},
     { title: "Educational Support Tools", image: "/assets/edu6.png", link: "/Etools", bg: "bg-blue-100", description: "Apps, software, and other tools for deaf-friendly education.", imgSize: "w-48 h-48" },
    
   ];
